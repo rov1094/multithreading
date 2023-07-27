c9627a92b80d5399dd2a35402ab0bfdcd961303f
Rahul Verma <rv@example.com>
2023-07-27 23:57:56 +0530

    Race Condition Problem arised

---
+++ src/main/java/memory/management/Main.java
@@ -0,0 +1,17 @@
+package memory.management;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x=1;
+        int y=2;
+        int result= sum(x,y);
+    }
+
+    private static int sum(int x, int y) {
+        int s=x+y;
+        return s;
+    }
+
+}
---
+++ src/main/java/race/condition/Main.java
@@ -0,0 +1,69 @@
+package race.condition;
+
+/**
+ * Resource Sharing issue
+ */
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter=new InventoryCounter();
+        IncrementThread incrementThread=new IncrementThread(inventoryCounter);
+        DecrementThread decrementThread=new DecrementThread(inventoryCounter);
+        incrementThread.start();
+        decrementThread.start();
+
+        incrementThread.join();// we joined increment thread after starting decrement thread this will not give proper result
+        decrementThread.join();
+
+        System.out.println("Inventory Item in the stock are : "+inventoryCounter.getInventory()); // Error in result
+    }
+
+    private static class IncrementThread extends Thread{
+
+        private InventoryCounter inventoryCounter;
+
+        public IncrementThread(InventoryCounter inventoryCounter){
+            this.inventoryCounter=inventoryCounter;
+        }
+
+        @Override
+        public void run(){
+            for (int i=0;i<10000;i++){
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    private static class DecrementThread extends Thread{
+
+        private InventoryCounter inventoryCounter;
+
+        public DecrementThread(InventoryCounter inventoryCounter){
+            this.inventoryCounter=inventoryCounter;
+        }
+
+        @Override
+        public void run(){
+            for (int i=0;i<10000;i++){
+                inventoryCounter.decrement();
+            }
+        }
+
+    }
+
+    private static class InventoryCounter{
+        private int item;
+
+        public void increment(){
+            item++;
+        }
+
+        public void decrement(){
+            item--;
+        }
+
+        public int getInventory(){
+            return item;
+        }
+    }
+}
